71f39e6e8461156215448ba9c84697e9905db61c
priyabrata-cpp <76444246+priyabrata-cpp@users.noreply.github.com>
2023-01-04 11:15:32 +0530

    Create sumOfNaturalTill100.py

---
+++ sumOfNaturalTill100.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number: "))
+if num<0:
+  print("Please Enter a Positive Number)
+        
+        else:
+        sum = 0
+        while(num>0):
+        sum+=num
+        num-=1
+        print("The sum is: ",sum)
